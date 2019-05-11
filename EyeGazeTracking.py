fd4a61be2587aed97ceb26f3e1ab5f86ca284f71
Piotr Szubert <piotr.szubert1@wp.pl>
2019-05-11 20:05:37 +0200

    add neural network for pupil recognition

--- EyeGazeTracking.py
+++ EyeGazeTracking.py
@@ -3,7 +3,10 @@ import dlib
 import numpy as np
 from math import hypot
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(1)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+capture.set(cv2.CAP_PROP_FPS, 30)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
@@ -33,22 +36,22 @@ while (True):
         left_point = (landmarks.part(36).x, landmarks.part(36).y)
         right_point = (landmarks.part(39).x, landmarks.part(39).y)
 
-        hor_line = cv2.line(frame, left_point, right_point, (255, 0, 0), 1)
+        # hor_line = cv2.line(frame, left_point, right_point, (255, 0, 0), 1)
 
         srodek_gora_lewe = srodek(landmarks.part(37), landmarks.part(38))
         srodek_dol_lewe = srodek(landmarks.part(41), landmarks.part(40))
 
-        ver_line = cv2.line(frame, srodek_gora_lewe, srodek_dol_lewe, (255, 0, 0), 1)
+        # ver_line = cv2.line(frame, srodek_gora_lewe, srodek_dol_lewe, (255, 0, 0), 1)
 
         left_point1 = (landmarks.part(42).x, landmarks.part(42).y)
         right_point1 = (landmarks.part(45).x, landmarks.part(45).y)
 
-        hor_line1 = cv2.line(frame, left_point1, right_point1, (255, 0, 0), 1)
+        # hor_line1 = cv2.line(frame, left_point1, right_point1, (255, 0, 0), 1)
 
         srodek_gora_prawe = srodek(landmarks.part(43), landmarks.part(44))
         srodek_dol_prawe = srodek(landmarks.part(47), landmarks.part(46))
 
-        ver_line1 = cv2.line(frame, srodek_gora_prawe, srodek_dol_prawe, (255, 0, 0), 1)
+        # ver_line1 = cv2.line(frame, srodek_gora_prawe, srodek_dol_prawe, (255, 0, 0), 1)
 
         hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
         ver_line_lenght = hypot((srodek_gora_lewe[0] - srodek_dol_lewe[0]), (srodek_gora_lewe[1] - srodek_dol_lewe[1]))
@@ -60,6 +63,55 @@ while (True):
 
         if wsp_otwartosci_oka > 6:
             cv2.putText(frame, "Zakmniete", (50, 150), font, 3, (255, 0, 0))
+
+        left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                                    (landmarks.part(37).x, landmarks.part(37).y),
+                                    (landmarks.part(38).x, landmarks.part(38).y),
+                                    (landmarks.part(39).x, landmarks.part(39).y),
+                                    (landmarks.part(40).x, landmarks.part(40).y),
+                                    (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+
+        # cv2.polylines(frame, [left_eye_region], True, (0, 0, 255), 2)
+
+        right_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                                    (landmarks.part(37).x, landmarks.part(37).y),
+                                    (landmarks.part(38).x, landmarks.part(38).y),
+                                    (landmarks.part(39).x, landmarks.part(39).y),
+                                    (landmarks.part(40).x, landmarks.part(40).y),
+                                    (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+
+        # cv2.polylines(frame, [right_eye_region], True, (0, 0, 255), 2)
+
+        height, width,_ = frame.shape
+        mask = np.zeros((height, width), np.uint8)
+
+        cv2.polylines(mask, [left_eye_region], True, 255, 2)
+        cv2.fillPoly(mask, [left_eye_region], 255)
+        left_eye = cv2.bitwise_and(gray,gray, mask=mask)
+
+        min_x = np.min(left_eye_region[:, 0])
+        max_x = np.max(left_eye_region[:, 0])
+
+        min_y = np.min(left_eye_region[:, 1])
+        max_y = np.max(left_eye_region[:, 1])
+
+        gray_eye = left_eye[min_y: max_y, min_x: max_x]
+        _,threshold_eye = cv2.threshold(gray_eye,50,240,cv2.THRESH_BINARY)
+        # _, contours, _ = cv2.findContours(threshold_eye, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # for cnt in contours:
+        #     cv2.drawContours(threshold_eye, cnt, 0, (0, 255, 0), 3)
+        # # gray_eye = cv2.medianBlur(gray_eye, 5)
+        circles = cv2.HoughCircles(threshold_eye,cv2.HOUGH_GRADIENT,0.5, 41, param1=70, param2=30, minRadius=10,maxRadius=175)
+
+        if circles is not None:
+            for i in circles[0, :]:
+                cv2.circle(threshold_eye, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                cv2.circle(threshold_eye, (i[0], i[1]), 2, (0, 0, 255), 3)
+        # threshold_eye = cv2.resize(threshold_eye, None, fx=5, fy=5)
+
+
+        cv2.imshow("Eye", threshold_eye)
+        cv2.imshow("Eyess", gray_eye)
 
     cv2.imshow("Frame", frame)
 
---
+++ EyeGazeTracking_v1.py
@@ -0,0 +1,221 @@
+from ctypes import *
+import math
+import random
+import os
+import cv2
+import numpy as np
+import time
+import darknet
+from matplotlib import pyplot as plt
+import dlib
+from math import hypot
+
+
+def srodek(p1, p2):
+    return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
+
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+
+def cvDrawBoxes(detections, img):
+    xmin=0
+    ymin=0
+    xmax=0
+    ymax=0
+    print(detections)
+    for detection in detections:
+        x, y, w, h = detection[2][0],\
+            detection[2][1],\
+            detection[2][2],\
+            detection[2][3]
+        xmin, ymin, xmax, ymax = convertBack(
+            float(x), float(y), float(w), float(h))
+        pt1 = (xmin, ymin)
+        pt2 = (xmax, ymax)
+        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+        cv2.putText(img,
+                    detection[0].decode() +
+                    " [" + str(round(detection[1] * 100, 2)) + "]",
+                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    [0, 255, 0], 2)
+    return img, xmin, ymin, xmax, ymax
+    
+
+netMain = None
+metaMain = None
+altNames = None
+
+def eyeGazeTacking():
+
+    global metaMain, netMain, altNames
+    configPath = "cfg/yolov3-tiny-1c.cfg"
+    weightPath = "backup/yolov3-tiny-1c_last.weights"
+    metaPath = "data/eye.data"
+   
+    if netMain is None:
+        netMain = darknet.load_net_custom(configPath.encode(
+            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+    if metaMain is None:
+        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+    if altNames is None:
+        try:
+            with open(metaPath) as metaFH:
+                metaContents = metaFH.read()
+                import re
+                match = re.search("names *= *(.*)$", metaContents,
+                                  re.IGNORECASE | re.MULTILINE)
+                if match:
+                    result = match.group(1)
+                else:
+                    result = None
+                try:
+                    if os.path.exists(result):
+                        with open(result) as namesFH:
+                            namesList = namesFH.read().strip().split("\n")
+                            altNames = [x.strip() for x in namesList]
+                except TypeError:
+                    pass
+        except Exception:
+            pass
+
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    cap.set(cv2.CAP_PROP_FPS, 30)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # Create an image we reuse for each detect
+    darknet_image = darknet.make_image(darknet.network_width(netMain),
+                                    darknet.network_height(netMain),3)
+    while True:
+        stime = time.time()
+        ret, image = cap.read()
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray)
+
+
+        for face in faces:
+
+            # x, y = face.left(), face.top()
+            # x1, y1 = face.right(), face.bottom()
+            # cv2.rectangle(frame, (x,y), (x1,y1), (0,255,0), 2)
+            landmarks = predictor(gray, face)
+
+            # for points in range(36, 48):
+            #   x = landmarks.part(points).x
+            #  y = landmarks.part(points).y
+            # cv2.circle(frame, (x, y), 3, (0, 0, 255), 2)
+            left_point = (landmarks.part(36).x, landmarks.part(36).y)
+            right_point = (landmarks.part(39).x, landmarks.part(39).y)
+
+            # hor_line = cv2.line(image, left_point, right_point, (0, 0, 255), 3)
+
+            srodek_gora_lewe = srodek(landmarks.part(37), landmarks.part(38))
+            srodek_dol_lewe = srodek(landmarks.part(41), landmarks.part(40))
+
+            # ver_line = cv2.line(image, srodek_gora_lewe, srodek_dol_lewe, (0, 0, 255), 3)
+
+            left_point1 = (landmarks.part(42).x, landmarks.part(42).y)
+            right_point1 = (landmarks.part(45).x, landmarks.part(45).y)
+
+            # hor_line1 = cv2.line(image, left_point1, right_point1, (0, 0, 255), 3)
+
+            srodek_gora_prawe = srodek(landmarks.part(43), landmarks.part(44))
+            srodek_dol_prawe = srodek(landmarks.part(47), landmarks.part(46))
+
+            # ver_line1 = cv2.line(image, srodek_gora_prawe, srodek_dol_prawe, (0, 0, 255), 3)
+
+            hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+            ver_line_lenght = hypot((srodek_gora_lewe[0] - srodek_dol_lewe[0]),
+                                    (srodek_gora_lewe[1] - srodek_dol_lewe[1]))
+
+            wsp_otwartosci_oka = hor_line_lenght / ver_line_lenght
+
+            if wsp_otwartosci_oka < 6:
+                cv2.putText(image, "Otwarte", (50, 150), font, 3, (255, 0, 0))
+
+            if wsp_otwartosci_oka > 6:
+                cv2.putText(image, "Zakmniete", (50, 150), font, 3, (255, 0, 0))
+
+            left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                                        (landmarks.part(37).x, landmarks.part(37).y),
+                                        (landmarks.part(38).x, landmarks.part(38).y),
+                                        (landmarks.part(39).x, landmarks.part(39).y),
+                                        (landmarks.part(40).x, landmarks.part(40).y),
+                                        (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+
+            # cv2.polylines(image, [left_eye_region], True, (0, 0, 255), 2)
+
+            right_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                                         (landmarks.part(37).x, landmarks.part(37).y),
+                                         (landmarks.part(38).x, landmarks.part(38).y),
+                                         (landmarks.part(39).x, landmarks.part(39).y),
+                                         (landmarks.part(40).x, landmarks.part(40).y),
+                                         (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+
+            # cv2.polylines(frame, [right_eye_region], True, (0, 0, 255), 2)
+
+            # height, width, _ = image.shape
+            # mask = np.zeros((height, width), np.uint8)
+            #
+            # # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            #
+            # cv2.polylines(mask, [left_eye_region], True, 255, 2)
+            #
+            # min_x = np.min(left_eye_region[:, 0])
+            # max_x = np.max(left_eye_region[:, 0])
+            #
+            # min_y = np.min(left_eye_region[:, 1])
+            # max_y = np.max(left_eye_region[:, 1])
+            #
+            # gray_eye = image[min_y: max_y, min_x: max_x]
+            # _, threshold_eye = cv2.threshold(gray_eye, 50, 240, cv2.THRESH_BINARY)
+            color_eye = image
+            color_eye = cv2.cvtColor(color_eye, cv2.COLOR_BGR2RGB)
+            color_eye = cv2.resize(color_eye,
+                               (darknet.network_width(netMain),
+                                darknet.network_height(netMain)),
+                               interpolation=cv2.INTER_LINEAR)
+
+            darknet.copy_image_from_bytes(darknet_image, color_eye.tobytes())
+
+            detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+            _, xmin, ymin, xmax, ymax = cvDrawBoxes(detections, color_eye)
+            xsr = int(round(xmin + ((xmax-xmin)/2)))
+            ysr = int(round(ymin + ((ymax-ymin)/2)))
+            cv2.line(color_eye, (int(round(left_point1[0]/3.07)), int(round(left_point1[1]/1.73))), (int(round(right_point1[0]/3.07)), int(round(right_point1[1]/1.73))), (0, 0, 255), 2)
+            cv2.line(color_eye, (int(round(srodek_gora_prawe[0]/3.07)), int(round(srodek_gora_prawe[1]/1.73))), (int(round(srodek_dol_prawe[0]/3.07)),int(round(srodek_dol_prawe[1]/1.73))), (0, 0, 255), 2)
+            cv2.circle(color_eye, (xsr, ysr), 4, (255, 0, 0), 4)
+            color_eye = cv2.cvtColor(color_eye, cv2.COLOR_BGR2RGB)
+
+            cv2.imshow("Eyess", color_eye)
+
+        cv2.imshow("Frame", gray)
+
+        print('FPS {:.1f}'.format(1 / (time.time() - stime)))
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    
+
+if __name__ == "__main__":
+    eyeGazeTacking()
+
+
+
+
+
+
+
+
